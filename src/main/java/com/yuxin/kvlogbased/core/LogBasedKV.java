00cd34b116d2c29febccb490d3b50f56bc3d28e5
Yuxin Shang <yxshang97@gmail.com>
2023-03-15 22:31:16 -0400

    version2.0

---
+++ src/main/java/com/yuxin/kvlogbased/core/LogBasedKV.java
@@ -0,0 +1,102 @@
+package com.yuxin.kvlogbased.core;
+
+import com.yuxin.kvlogbased.api.KVClient;
+import com.yuxin.kvlogbased.enums.CommandTypeEnum;
+import com.yuxin.kvlogbased.model.CommandRequestModel;
+
+import java.io.*;
+
+public class LogBasedKV implements KVClient {
+    private File logFile;
+
+    public LogBasedKV(String fileName) {
+        logFile = new File(fileName);
+    }
+
+    @Override
+    public void put(String key, Object value) {
+        ObjectOutputStream oos = null;
+
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(logFile));
+
+            CommandRequestModel commandRequestModel = new CommandRequestModel();
+            commandRequestModel.setCommandTypeEnum(CommandTypeEnum.SET);
+            commandRequestModel.setKey(key);
+            commandRequestModel.setValue(value);
+
+            oos.writeObject(commandRequestModel);
+            oos.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("put exception,[" + key + "," + value + "]");
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+            } catch (Exception e) {
+                System.out.println("objectOutputStream close exception");
+            }
+        }
+    }
+
+    @Override
+    public Object get(String key) {
+        ObjectInputStream ois = null;
+        try {
+            FileInputStream fis = new FileInputStream(logFile);
+            ois = new ObjectInputStream(new FileInputStream(logFile));
+            CommandRequestModel lastUpdateCommand = null;
+            CommandRequestModel curCommand = (CommandRequestModel) ois.readObject();
+            while (curCommand != null) {
+                if (key.equals(curCommand.getKey())) {
+                    lastUpdateCommand = curCommand;
+                }
+                curCommand = (CommandRequestModel) ois.readObject();
+            }
+
+            if (lastUpdateCommand == null || lastUpdateCommand.getCommandTypeEnum() == CommandTypeEnum.DEL) {
+                return null;
+            }
+            return lastUpdateCommand.getValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("get exception,[" + key + "]");
+        } finally {
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (Exception e) {
+                System.out.println("objectInputStream close exception");
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void del(String key) {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream(logFile));
+
+            CommandRequestModel commandRequestModel = new CommandRequestModel();
+            commandRequestModel.setCommandTypeEnum(CommandTypeEnum.DEL);
+            commandRequestModel.setKey(key);
+
+            oos.writeObject(commandRequestModel);
+            oos.flush();
+        } catch (Exception e) {
+            System.out.println("del exception,[" + key + "]");
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+            } catch (Exception e) {
+                System.out.println("objectOutputStream close exception");
+            }
+        }
+    }
+}
